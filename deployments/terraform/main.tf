9e3a716f0f1943a04ff656e57457e4c8532ff546
EC2 Default User <ec2-user@ip-172-31-11-17.ec2.internal>
2020-08-07 15:14:20 +0000

    Jenkins file changed and readdu to use

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,9 +1,16 @@
-provider "aws" {}
-resource "aws_instance" "jenkins" {
-    ami = "ami-08f3d892de259504d"
-    instance_type = "t2.micro"
-
-    tags = {
-      Name = "Jenkins"
-    }
+provider "aws" {
+  version = "2.59"
+  region = "us-east-1"                                                              when we pramatirized region we dont need this line 
 }
+
+resource "aws_instance" "jenkins" {               
+   
+  ami = "${var.ami}"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.Name}"
+  }
+}
+
+variable "ami"{}
+variable "Name"{}
